355d45aab3e0334e8435cb1f8e1e1d0f68973ef4
Bria White <bria.awhite@gmail.com>
2020-08-08 13:07:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,12 @@ class Shoe
   
   BRAND = []
 
-  
+  def initialize(brand)
+    @brand = brand
+    if BRANDS.include?(@brand)
+      BRANDS << @brand
+    end
+  end
 
   def cobble
     self.condition = "new"
